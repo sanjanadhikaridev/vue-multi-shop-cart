b5796b473d757825d3c03d3946415ab8abc25625
sanjan274 <adhikari.sanjan@crowdbotics.com>
2020-05-28 13:30:58 +0800

    update

--- src/router/router.ts
+++ src/router/router.ts
@@ -7,27 +7,27 @@ import ShoppingCart from '@/views/ShoppingCart.vue'
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      redirect: '/products/all'
-    },
-    {
-      path: '/products/all',
-      name: 'products',
-      component: Products
-    },
-    {
-      path: '/products/:id',
-      name: 'product-detail',
-      // component: () => import(/* webpackChunkName: "about" */ './views/ProductDetail.vue')
-      component: ProductDetail,
-      props: true
-    },
-    {
-      path: '/order/summary',
-      name: 'order-summary',
-      component: ShoppingCart
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            redirect: '/products/all'
+        },
+        {
+            path: '/products/all',
+            name: 'products',
+            component: Products
+        },
+        {
+            path: '/products/:id',
+            name: 'product-detail',
+            // component: () => import(/* webpackChunkName: "about" */ './views/ProductDetail.vue')
+            component: ProductDetail,
+            props: true
+        },
+        {
+            path: '/order/summary',
+            name: 'order-summary',
+            component: ShoppingCart
+        }
+    ]
 })
